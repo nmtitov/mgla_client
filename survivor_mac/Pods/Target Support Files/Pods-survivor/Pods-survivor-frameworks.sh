e92b0d2ab2d93bc4c9fa81663dc12f5e9fd0c83c
Nikita Titov <nmtitov@yandex.ru>
2018-01-05 00:40:19 +0200

    Revert "Install Cocoalumberjack"
    This reverts commit 1cbc10140424fc2d09cf7f2227209a973da4ffca.

--- survivor_mac/Pods/Target Support Files/Pods-survivor/Pods-survivor-frameworks.sh
+++ survivor_mac/Pods/Target Support Files/Pods-survivor/Pods-survivor-frameworks.sh
@@ -102,12 +102,10 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/CocoaLumberjack/CocoaLumberjack.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Decodable/Decodable.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Starscream/Starscream.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/CocoaLumberjack/CocoaLumberjack.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Decodable/Decodable.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Starscream/Starscream.framework"
 fi
