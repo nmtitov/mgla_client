6db06227b877c756b7eeebc484e38bb0a0100c45
Nikita Titov <nmtitov@yandex.ru>
2018-01-24 12:59:51 +0200

    Keep TTY logs

--- survivor_mac/survivor/AppDelegate.swift
+++ survivor_mac/survivor/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         func setupLogging() {
             DDLog.add(DDTTYLogger.sharedInstance) // TTY = Xcode console
-            DDLog.add(DDASLLogger.sharedInstance) // ASL = Apple System Logs
+//            DDLog.add(DDASLLogger.sharedInstance) // ASL = Apple System Logs
             
             let fileLogger: DDFileLogger = DDFileLogger() // File Logger
             fileLogger.rollingFrequency = TimeInterval(60*60*24)  // 24 hours
