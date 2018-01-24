25a4c2082042317d46149271c3c48e9baa580aba
Nikita Titov <nmtitov@yandex.ru>
2018-01-24 12:29:17 +0200

    Change port, add remote url

--- survivor_mac/survivor/Service/WebSocketService.swift
+++ survivor_mac/survivor/Service/WebSocketService.swift
@@ -32,7 +32,8 @@ class WebSocketService: WebSocketDelegate {
     }
     
     init() {
-        let url = URL(string: "ws://localhost:8080/websocket")!
+        let url = URL(string: "ws://localhost:9000/websocket")!
+//        let url = URL(string: "ws://titov.link:9000/websocket")!
         let request = URLRequest(url: url)
         socket = WebSocket(request: request)
         socket.delegate = self
