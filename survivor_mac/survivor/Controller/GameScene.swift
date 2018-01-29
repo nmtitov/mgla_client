12c059953c659baf17b90219f2b8ccaead5367cd
Nikita Titov <nmtitov@yandex.ru>
2018-01-29 22:24:49 +0200

    Rename actionTeleport -> actionInput

--- survivor_mac/survivor/Controller/GameScene.swift
+++ survivor_mac/survivor/Controller/GameScene.swift
@@ -43,7 +43,7 @@ class GameScene: SKScene, Ensurable {
     }
     
     func touchDown(atPoint point: CGPoint) {
-        AppDelegate.shared.webSocketService.actionTeleport(point: point)
+        AppDelegate.shared.webSocketService.actionInput(point: point)
     }
     
     func touchMoved(toPoint point: CGPoint) {
